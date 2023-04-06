b576adf8ba9a045a412d096f834baf6a415a20c7
Juna Branca <junabranca@gmail.com>
2023-04-06 00:30:26 +0000

    Delete Sock (Backend)

--- sock_service/routers/sock.py
+++ sock_service/routers/sock.py
@@ -27,3 +27,17 @@ async def create_sock(
 ):
     user_id = account_data["id"]
     return socks.create(info, user_id)
+
+@router.delete("/api/socks/{sock_id}", response_model=bool | dict | HttpError | Error)
+def delete_sock(
+    sock_id: int,
+    response: Response,
+    user_id: int,
+    socks: SockQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+) -> bool:
+    if account_data["id"] == user_id:
+        return socks.delete(sock_id, user_id)
+    elif account_data["id"] != user_id:
+        response.status_code = 400
+        return {"Error" : "Unable to delete other user's socks"}
