57bd875f1db02065a4d5ef777d77686612329ba9
Juna Branca <junabranca@gmail.com>
2023-04-15 08:38:13 +0000

    Fixed Delete Match so that only the approving_user or receiving_user can delete

--- sock_service/routers/match.py
+++ sock_service/routers/match.py
@@ -50,11 +50,13 @@ def get_matches_by_user(
 def delete_match(
     match_id: int,
     response: Response,
+    requesting_user:int,
+    approving_user:int,
     matches: MatchQueries = Depends(),
     account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> bool:
-        try:
-            return matches.delete(match_id)
-        except:
+        if requesting_user == account_data["id"] or approving_user == account_data["id"]:
+            return matches.delete(match_id, requesting_user, approving_user)
+        else:
             response.status_code = 400
             return {"Error" : "Unable to delete match"}
